2696a8bd292e35b47ba57da07b5c3d60cb5f8d62
Samruddhi Khandale <samruddhikhandale@github.com>
2023-03-28 21:12:27 +0000

    create kind cluster & install serving and ingress

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+set -eux
+
+# create registry container unless it already exists
+reg_name='kind-registry'
+reg_port='5001'
+if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
+    registry:2
+fi
+
+# create a cluster with the local registry enabled in containerd
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
+    endpoint = ["http://${reg_name}:5000"]
+EOF
+
+# connect the registry to the cluster network if not already connected
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
+  docker network connect "kind" "${reg_name}"
+fi
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
+
+export KO_DOCKER_REPO=kind.local
+
+echo "Deploying cert-manager..."
+kubectl apply -f ./third_party/cert-manager-latest/cert-manager.yaml
+kubectl wait --for=condition=Established --all crd
+kubectl wait --for=condition=Available -n cert-manager --all deployments
+
+echo "Deploying Knative Serving..."
+ko apply --selector knative.dev/crd-install=true -Rf config/core/
+kubectl wait --for=condition=Established --all crd
+ko apply -Rf config/core/
+
+echo "Deploying Knative ingress with Kourier..."
+kubectl apply -f ./third_party/kourier-latest/kourier.yaml
+kubectl patch configmap/config-network \
+  -n knative-serving \
+  --type merge \
+  -p '{"data":{"ingress.class":"kourier.ingress.networking.knative.dev"}}'
+
+

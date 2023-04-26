89e23857f90321ddc6a8512f154b68cac7c032ec
Samruddhi Khandale <samruddhikhandale@github.com>
2023-04-26 00:14:30 +0000

    update dir

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -23,6 +23,7 @@ until docker info > /dev/null 2>&1 ; do
   sleep 0.1
 done
 
+CURRENT_DIR=$(pwd)
 cd /
 
 echo "Setting up local container registry..."
@@ -98,21 +99,21 @@ metadata:
 EOF
 
 echo "Deploying cert-manager..."
-kubectl apply -f $CODESPACE_VSCODE_FOLDER/third_party/cert-manager-latest/cert-manager.yaml
+kubectl apply -f $CURRENT_DIR/third_party/cert-manager-latest/cert-manager.yaml
 kubectl wait --for=condition=Established --all crd
 kubectl wait --for=condition=Available -n cert-manager --all deployments
 
 echo "Deploying Knative Serving..."
-ko apply --selector knative.dev/crd-install=true -Rf $CODESPACE_VSCODE_FOLDER/config/core/
+ko apply --selector knative.dev/crd-install=true -Rf $CURRENT_DIR/config/core/
 kubectl wait --for=condition=Established --all crd
-ko apply -Rf $CODESPACE_VSCODE_FOLDER/config/core/
+ko apply -Rf $CURRENT_DIR/config/core/
 
 echo "Setting up sslip.io domain name"
-ko delete -f $CODESPACE_VSCODE_FOLDER/config/post-install/default-domain.yaml --ignore-not-found
-ko apply -f $CODESPACE_VSCODE_FOLDER/config/post-install/default-domain.yaml
+ko delete -f $CURRENT_DIR/config/post-install/default-domain.yaml --ignore-not-found
+ko apply -f $CURRENT_DIR/config/post-install/default-domain.yaml
 
 echo "Deploying Knative ingress with Kourier..."
-kubectl apply -f $CODESPACE_VSCODE_FOLDER/third_party/kourier-latest/kourier.yaml
+kubectl apply -f $CURRENT_DIR/third_party/kourier-latest/kourier.yaml
 kubectl patch configmap/config-network \
   -n knative-serving \
   --type merge \
